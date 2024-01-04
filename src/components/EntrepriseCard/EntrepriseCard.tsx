d37626a09abdafc0b3c678d174d6e857074bf94e
Alain Chea <alain.chea@efrei.net>
2024-01-04 01:20:16 +0100

    feat(components): card entreprise

---
+++ src/components/EntrepriseCard/EntrepriseCard.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "./EntrepriseCard.module.scss";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+import Location from "@/assets/vectors/location.svg";
+import Sector from "@/assets/vectors/sector.svg";
+import Size from "@/assets/vectors/people.svg";
+import Link from "next/link";
+
+type Props = {
+  banner: string | StaticImageData;
+  logo: string | StaticImageData;
+  name: string;
+  sector: string;
+  location: string;
+  size: string;
+};
+
+const EntrepriseCard = ({
+  banner,
+  logo,
+  name,
+  sector,
+  location,
+  size,
+}: Props) => {
+  return (
+    <div className={styles.container}>
+      <Link href={`entreprises/${name}`} className={styles.link}>
+        <div className={styles.banner}>
+          <Image
+            src={banner}
+            width={100}
+            height={100}
+            alt=""
+            className={styles.bannerImage}
+          />
+          <Image
+            src={logo}
+            width={100}
+            height={100}
+            alt=""
+            className={styles.logo}
+          />
+        </div>
+        <div className={styles.entrepriseInfo}>
+          <h3 className={styles.title}>{name}</h3>
+          <div className={styles.infoContainer}>
+            <div className={styles.infoContent}>
+              <Sector className={styles.logoInfo} />
+              <span className={styles.info}>{sector}</span>
+            </div>
+            <div className={styles.infoContent}>
+              <Location className={styles.logoInfo} />
+              <span className={styles.info}>{location}</span>
+            </div>
+            <div className={styles.infoContent}>
+              <Size className={styles.logoInfo} />
+              <span className={styles.info}>{size}</span>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default EntrepriseCard;

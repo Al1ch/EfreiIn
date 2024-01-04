95b0021ec5f83cbf8fb515add9774313f0d13855
Alain Chea <alain.chea@efrei.net>
2024-01-04 18:25:24 +0100

    feat(page): add filter drop down

--- src/app/entreprises/page.tsx
+++ src/app/entreprises/page.tsx
@@ -3,9 +3,31 @@ import styles from "./page.module.scss";
 import EntrepriseCard from "@/components/EntrepriseCard/EntrepriseCard";
 import sgBanner from "@/assets/images/sg-banner.jpg";
 import sgLogo from "@/assets/images/sgLogo.png";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import FilterDropDown from "@/components/FilterDropDown/FilterDropDown";
 export default async function EntreprisePage() {
   return (
     <div className={styles.container}>
+      <div className={styles.researchFilter}>
+        <SearchBar />
+        <FilterDropDown
+          title="Secteur"
+          options={["Banque", "Assurance", "Finance"]}
+        />
+        <FilterDropDown
+          title="Secteur"
+          options={["Banque", "Assurance", "Finance"]}
+        />
+        <FilterDropDown
+          title="Secteur"
+          options={["Banque", "Assurance", "Finance"]}
+        />
+
+        {/* <div className={styles.location}>Localisation</div>
+        <div>Secteur</div>
+        <div>Taille</div> */}
+      </div>
+
       <div className={styles.cardListContainer}>
         <EntrepriseCard
           banner={sgBanner}

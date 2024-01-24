6b1e1dfc2fa01e74b97b26e259010a8403f22530
dzungpnguyen <thienhoangphuongdung@gmail.com>
2024-01-24 23:32:37 +0100

    radar chart in AboutTab

--- src/components/AboutTab/AboutTab.tsx
+++ src/components/AboutTab/AboutTab.tsx
@@ -1,11 +1,102 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./AboutTab.module.scss";
 import Link from "next/link";
+import Chart from "chart.js/auto";
 
 // const AboutTab = () => {
 const AboutTab: React.FC<{ data: any }> = ({ data }) => {
 
   console.log('AboutTab ENTREPRISE DATA:', data)
+
+
+  function normalizeData(numbers: string[]): number[] {
+    const numericValues = numbers.map((num) => parseFloat(num));
+
+    const minValue = Math.min(...numericValues);
+    const maxValue = Math.max(...numericValues);
+
+    const normalizedAndRounded = numericValues.map((value) => {
+      const normalized = (value - minValue) / (maxValue - minValue) * 4 + 1;
+      const rounded = Math.round(normalized * 2) / 2; // Round to the nearest 0.5
+      return normalized;
+    });
+  
+    return normalizedAndRounded;
+  }
+
+  function addZeroAfterEachNumber(numbers: number[]): number[] {
+    const result: number[] = [];
+  
+    for (const num of numbers) {
+      result.push(num, 0);
+    }
+  
+    return result;
+  }
+
+  const radarChartRef = useRef<HTMLCanvasElement | null>(null);
+  const chartInstanceRef = useRef<Chart | null>(null);
+
+  useEffect(() => {
+    if (radarChartRef.current) {
+      const ctx = radarChartRef.current.getContext("2d");
+      
+      // Destroy existing chart instance
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+
+      // Extract the relevant data for the radar chart
+      // Normalize data
+      const normalizedData = normalizeData(
+        [
+          // data.column_values[2].text,     // effectif (chiffre trop grande => bye)
+          // data.column_values[6].text,     // tax d'apprentissage (chiffre trop grande => bye)
+          data.column_values[7].text,     // nb stagiaires
+          data.column_values[8].text,     // nb alternants
+          data.column_values[9].text,     // nb alumnis
+          data.column_values[10].text,    // nb actions realisees
+        ]
+      );
+
+      const normalizedDataWithZeros = addZeroAfterEachNumber(normalizedData);
+
+      console.log('NORMALIZE DATA', normalizedDataWithZeros)
+
+      const radarChartData = {
+        labels: ["Nombre de stagiaires", "", "Nombre d'alternants", "", "Nombre d'alumnis", "", "Nombre d'actions réalisées", ""],
+        datasets: [
+          {
+            label: "Radar Chart",
+            data: normalizedDataWithZeros.map(n => n === 0 ? null : n),
+            // data: [1,6,4,2,5,3],
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      // Create new chart instance
+      chartInstanceRef.current = new Chart(ctx, {
+        type: "radar",
+        data: radarChartData,
+        options: {
+          spanGaps: true,
+          scales: {
+            r: {
+              min: 0,
+              max: 5,
+              ticks: {
+                stepSize: 1,
+                display: false,
+              }
+            },
+          },
+        },
+      });
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -27,7 +118,13 @@ const AboutTab: React.FC<{ data: any }> = ({ data }) => {
           <span className={styles.text}>{data.column_values[2].text} employés</span>
         </div>
       </div>
+
+      {/* Show radar chart here */}
+      <div className={styles.chartContainer}>
+        <canvas ref={radarChartRef}></canvas>
+      </div>
     </div>
+
   );
 };
 

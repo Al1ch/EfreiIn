395a76c14129944b76ced12124a41b435243f3e2
dzungpnguyen <thienhoangphuongdung@gmail.com>
2024-01-24 22:26:24 +0100

    pass data to company banner

--- src/components/CompanyBanner/CompanyBanner.tsx
+++ src/components/CompanyBanner/CompanyBanner.tsx
@@ -1,26 +1,46 @@
 import React from "react";
 import styles from "./CompanyBanner.module.scss";
 import sgBanner from "@/assets/images/sg-banner.jpg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import sgLogo from "@/assets/images/sgLogo.png";
+import { describe } from "node:test";
 
-const CompanyBanner = () => {
+
+type Props = {
+  id: String;
+  banner: string | StaticImageData;
+  logo: string | StaticImageData;
+  name: string;
+  description: string;
+  details: string;
+};
+
+const CompanyBanner = ({
+  id,
+  banner,
+  logo,
+  name,
+  description,
+  details,
+}: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.bannerContainer}>
         <div className={styles.banner}>
-          <Image src={sgBanner} alt="sgBanner" className={styles.bannerImage} />
-          <Image src={sgLogo} alt="sgLogo" className={styles.logo} />
+          <Image src={banner} alt="sgBanner" className={styles.bannerImage} />
+          <Image src={logo} alt="sgLogo" className={styles.logo} />
         </div>
       </div>
       <div className={styles.companiesInfo}>
         <div>
-          <h1 className={styles.title}> Société Générale</h1>
-          <p className={styles.description}>{`c'est vous l'avenir`}</p>
+          <h1 className={styles.title}>{name}</h1>
+          {/* <p className={styles.description}>{`c'est vous l'avenir`}</p> */}
+          <p className={styles.description}>{description}</p>
         </div>
-        <p className={styles.details}>
+        {/* <p className={styles.details}>
           Services bancaires - Paris - 44 017 employés
-        </p>
+        </p> */}
+        <p className={styles.details}>{details}</p>
       </div>
     </div>
   );
